f48dee5eebdaa1236cc909e7c24001c81647794b
Melike Şahin <melsaahin@gmail.com>
2025-05-17 00:51:48 +0300

    v12- win with eggs

--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     [SerializeField] private int _maxEggCount;
     private int _currentEggCount;
+    [SerializeField] private EggCounterUI _eggCounterUI;
 
     private void Awake()
     {
@@ -13,11 +14,13 @@ public class GameManager : MonoBehaviour
     public void OnEggCollected()
     {
         _currentEggCount++;
+         _eggCounterUI.SetEggCounterText(_currentEggCount,  _maxEggCount);
 
 
         if (_currentEggCount == _maxEggCount)
         {
             //win
+            _eggCounterUI.SetEggCompleted();
         }
         
     }
